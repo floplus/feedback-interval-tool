a24c6a6c72167a4188f7ae034c30f97a41416013
Florian Beyerlein <florian.beyerlein@hmmh.de>
2016-04-16 18:54:32 +0200

    added rabbitmq bundle and homestead install commands

--- after.sh
+++ after.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y update
+echo 'Installing RabbitMQ';
 
+echo 'deb http://www.rabbitmq.com/debian/ stable main' |
+        sudo tee /etc/apt/sources.list.d/rabbitmq.list
+
+wget -O- https://www.rabbitmq.com/rabbitmq-signing-key-public.asc |
+        sudo apt-key add -
+
+sudo apt-get -y update
+sudo apt-get -y install rabbitmq-server
+
+sudo apt-get -y install php-bcmath
 sudo apt-get -y install php-intl
